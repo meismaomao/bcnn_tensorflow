daedca4dfd95728f58bd31226b9a1d4698702e92
meismaomao <yqljxr@163.com>
2017-12-28 10:49:43 +0800

    Signed-off-by: meismaomao <yqljxr@163.com>

---
+++ bcnn_model1.py
@@ -0,0 +1,68 @@
+from dann_utils import *
+import tensorflow as tf
+
+class BcnnModel(object):
+    """Simple MNIST Bilinear CNN Model -- ONE CNN Network, Two CNN Data Stream!!!!!!"""
+    def __init__(self, batch_size, pixel_mean):
+        self.pixel_mean = pixel_mean
+        self.batch_size = batch_size
+        self._build_model()
+
+    def _build_model(self):
+        self.X = tf.placeholder(tf.uint8, [None, 28, 28, 3])
+        self.y = tf.placeholder(tf.float32, [None, 10])
+        self.domain = tf.placeholder(tf.float32, [None, 2])
+        self.l = tf.placeholder(tf.float32, [])
+        self.train = tf.placeholder(tf.bool, [])
+        self.disc = tf.placeholder(tf.float32, [None, 2])
+
+        X_input = (tf.cast(self.X, tf.float32) - self.pixel_mean) / 255.
+
+        # CNN model for feature extraction
+        with tf.variable_scope('feature_extractor1'):
+            W_conv0 = weight_variable([5, 5, 3, 32])
+            b_conv0 = bias_variable([32])
+            h_conv0 = tf.nn.relu(conv2d(X_input, W_conv0) + b_conv0)
+            h_pool0 = max_pool_2x2(h_conv0)
+
+            W_conv1 = weight_variable([5, 5, 32, 48])
+            b_conv1 = bias_variable([48])
+            h_conv1 = tf.nn.relu(conv2d(h_pool0, W_conv1) + b_conv1)
+            h_pool1 = max_pool_2x2(h_conv1)
+
+
+            conv1 = tf.transpose(h_pool1, perm=[0, 3, 1, 2])
+            conv1 = tf.reshape(conv1, [-1, 48, 14 * 14])
+            conv1_T = tf.transpose(conv1, perm=[0, 2, 1])
+
+            phi_I = tf.matmul(conv1, conv1_T)
+
+            phi_I = tf.reshape(phi_I, [-1, 48 * 48])
+
+            phi_I = tf.divide(phi_I, 196.0)
+
+            y_sqrt = tf.multiply(tf.sign(phi_I), tf.sqrt(tf.abs(phi_I) + 1e-12))
+
+            self.z_l2 = tf.nn.l2_normalize(y_sqrt, dim=1)
+
+
+        # Bilinear vector for class prediction
+        with tf.variable_scope('label_predictor'):
+
+            W_fc0 = weight_variable([48 * 48, 100])
+            b_fc0 = bias_variable([100])
+            h_fc0 = tf.nn.relu(tf.matmul(self.z_l2, W_fc0) + b_fc0)
+
+            W_fc1 = weight_variable([100, 100])
+            b_fc1 = bias_variable([100])
+            self.visual_feature = tf.matmul(h_fc0, W_fc1) + b_fc1
+            h_fc1 = tf.nn.relu(tf.matmul(h_fc0, W_fc1) + b_fc1)
+
+            W_fc2 = weight_variable([100, 10])
+            b_fc2 = bias_variable([10])
+            logits = tf.matmul(h_fc1, W_fc2) + b_fc2
+
+            self.pred = tf.nn.softmax(logits)
+            # print(logits, self.classify_labels)
+            self.pred_loss = tf.nn.softmax_cross_entropy_with_logits(labels=self.classify_labels, logits=logits)
+
